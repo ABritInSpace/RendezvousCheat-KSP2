239bdf93e5aa37920db1589d21cba2dae5e49818
ABritInSpace <68876225+ABritInSpace@users.noreply.github.com>
2023-02-25 22:35:57 +0000

    Added relevant acknowledgement of work modified from LazyOrbit

--- kspPhys/Rendezvous.cs
+++ kspPhys/Rendezvous.cs
@@ -11,6 +11,9 @@ using System.Runtime.CompilerServices;
 using KSP.Logging;
 using KSP.Sim;
 
+//WHERE INDICATED BY **1**, RELEVANT SECTIONS HAVE BEEN MODIFIED FROM LazyOrbit.
+//SEE README FOR DETAILS
+
 namespace kspPhys
 {
     [MainMod]
@@ -26,24 +29,31 @@ namespace kspPhys
         public bool doDraw = false;
         public override void Initialize()
         {
+            //**1**
             if (lod)
                 Destroy(this);
             lod = true;
+            //
         }
         void Awake()
         {
+            //**1**
             window = new Rect((Screen.width * 0.85f) - (75), (Screen.height / 2) - (300), 0, 0);
+            //
         }
         void Update()
         {
+            //**1**
             if (Input.GetKey(KeyCode.LeftAlt) && Input.GetKeyDown(KeyCode.R))
             {
                 doDraw = !doDraw;
             }
+            //
         }
 
         void OnGUI()
         {
+            //**1**
             if (doDraw)
             {
                 window = GUILayout.Window(GUIUtility.GetControlID(FocusType.Passive),
@@ -53,6 +63,7 @@ namespace kspPhys
                     GUILayout.Height(0),
                     GUILayout.Width(300));
             }
+            //
         }
 
         void draw(int id)
@@ -60,11 +71,15 @@ namespace kspPhys
             GameInstance game = GameManager.Instance.Game;
 
             GUILayout.BeginVertical();
-
+            
+            //**1**
             GUILayout.BeginHorizontal();
             GUILayout.Label("Distance, m: ", GUILayout.Width(75));
+            //
             displayDist = GUILayout.TextField(displayDist);
+            //**1**
             float.TryParse(displayDist, out dist);
+            //
             GUILayout.EndHorizontal();
             GUILayout.Label("Select a vessel to rendezvous with:");
 
@@ -72,8 +87,10 @@ namespace kspPhys
 
             VesselComponent[] vessels = game.SpaceSimulation.UniverseModel.GetAllVessels().ToArray();
             
+            //**1**
             GUILayout.BeginVertical(GUI.skin.GetStyle("Box"));
             scrollpos = GUILayout.BeginScrollView(scrollpos, false, true, GUILayout.Height(100), GUILayout.Width(150));
+            //
             foreach (VesselComponent v in vessels)
             {
                 if (GUILayout.Button(v.Name))
@@ -82,9 +99,11 @@ namespace kspPhys
                     DoRendz();
                 }
             }
+            //**1**
             GUILayout.EndScrollView();
             GUILayout.EndVertical();
             GUI.DragWindow(new Rect(0, 0, 10000, 20));
+            //
         }
 
         void DoRendz()
